b19c4a6c4cdb019f440e67a077df123c0cd1a300
Alvaro <alvaroawero10@gmail.com>
2020-08-04 13:30:59 -0400

    se migro a base de datos y se creo la app de Servicios

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ProyectoWebApp',
+    'servicios',
+    "django_readonly_field",
 ]
 
 MIDDLEWARE = [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-LA'
 
 TIME_ZONE = 'UTC'
 
